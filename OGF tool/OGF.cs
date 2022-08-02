08bbbf9ae4b5016f7d5a2a7312011a03a12ba2b6
VaIeroK <kondratenko449@gmail.com>
2022-08-02 13:22:40 +0300

    Почистил проект от мусора

--- OGF tool/OGF.cs
+++ OGF tool/OGF.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace OGF_tool
+namespace OGF_Tool
 {
     public enum OGF
     {
--- OGF tool/Properties/Resources.Designer.cs
+++ OGF tool/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OGF_tool.Properties {
+namespace OGF_Tool.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace OGF_tool.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OGF_tool.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OGF_Tool.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
